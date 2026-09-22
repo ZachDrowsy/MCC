4f5b25ace9be4cff6cdef30a59d95952c72625b3
Zachary Davidson <ztdavidson14@gmail.com>
2026-09-22 10:04:25 -0700

    Add commented Controller implementation scaffold

---
+++ Controller.cpp
@@ -0,0 +1,83 @@
+// Controller.cpp
+// Implement one outline at a time as we work through Controller.h.
+// Everything below is commented out for now, including the include.
+// Before enabling it, separate the shared definitions in States.h from
+// its declarations so main.ino and this file do not define them twice.
+
+/*
+#include "Controller.h"
+
+// Constructors
+
+Controller::Controller() {
+    // TODO
+}
+
+Controller::Controller(const AutoControl& A, const Schedule& S) {
+    // TODO
+}
+
+Controller::Controller(const Schedule& S) {
+    // TODO
+}
+
+Controller::Controller(const AutoControl& A) {
+    // TODO
+}
+
+// Manual request and final decision
+
+void Controller::setManual(bool on) {
+    // TODO
+}
+
+bool Controller::getManual() const {
+    // TODO
+}
+
+bool Controller::getShouldRun() const {
+    // TODO
+}
+
+// User settings
+
+bool Controller::setSchedule(int hour, int minute, int durationMinutes) {
+    // TODO
+}
+
+bool Controller::setScheduleDay(int day, bool enabled) {
+    // TODO
+}
+
+bool Controller::setAutomaticThresholds(float lower, float upper) {
+    // TODO
+}
+
+void Controller::setTurnOnBelow(bool enabled) {
+    // TODO
+}
+
+// Read settings
+
+const Schedule& Controller::getSchedule() const {
+    // TODO
+}
+
+const AutoControl& Controller::getAutomatic() const {
+    // TODO
+}
+
+// Update each kind of device
+
+void Controller::update(SolenoidState mode, float reading) {
+    // TODO
+}
+
+void Controller::update(LightState mode) {
+    // TODO
+}
+
+void Controller::update(DeviceState mode, float reading) {
+    // TODO
+}
+*/
